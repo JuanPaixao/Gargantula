5b3189eb0483aec4a334c98ef35a65228ccc67a1
Juan Paixao <juanpaixao@live.com>
2018-12-02 22:54:14 -0300

    Movimentação por física.

--- Assets/Scripts/Main Character/Player.cs
+++ Assets/Scripts/Main Character/Player.cs
@@ -5,44 +5,53 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public bool isOnDialogue = false;
+    private bool _isJumping;
+    public int numberOfJumps, maxJumps;
     public float speed, gravity, checkGround, jumpForce;
     public LayerMask groudLayerMask;
     public bool isGrounded;
     public GameObject groundDetector;
-    private CharacterController _controller;
+    private Rigidbody _rb;
     private RaycastHit _hit;
     [HideInInspector] public float movHor, movVer;
-    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 movement;
     private Transform _transform;
     void Start()
     {
         _transform = GetComponent<Transform>();
-        _controller = GetComponent<CharacterController>();
+        _rb = GetComponent<Rigidbody>();
+        maxJumps = 2;
     }
     void Update()
     {
-        Movement();
-        isGrounded = (Physics.Raycast(groundDetector.transform.position, Vector3.down, checkGround, groudLayerMask));
+        isGrounded = (Physics.Raycast(groundDetector.transform.position, Vector2.down, out _hit, checkGround, groudLayerMask));
+        if (isGrounded)
+        {
+            _isJumping = false;
+            numberOfJumps = 0;
+        }
+        MovementInputs();
     }
-    private void Movement()
+    void FixedUpdate()
     {
-        movHor = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        moveDirection = new Vector2(movHor, 0);
-
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            moveDirection.y = jumpForce;
-
-        }
-        if (isGrounded && !Input.GetKeyDown(KeyCode.Space))
-        {
-            moveDirection.y = 0;
-
-        }
-        moveDirection.y -= gravity * Time.deltaTime;
-        _controller.Move(moveDirection);
-
+        _rb.MovePosition(_rb.position + movement * Time.fixedDeltaTime);
+    }
+    private void MovementInputs()
+    {
+        movHor = Input.GetAxis("Horizontal") * speed;
+        movement = new Vector3(movHor, 0, 0);
         _transform.transform.eulerAngles = (movHor < 0) ? _transform.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, -109.66f, this.transform.eulerAngles.z) :
         _transform.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, 89.71101f, this.transform.eulerAngles.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (numberOfJumps < maxJumps || isGrounded)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, 0);
+                _rb.AddForce(Vector3.up * Mathf.Sqrt(jumpForce * -2 * Physics.gravity.y), ForceMode.VelocityChange);
+                _isJumping = true;
+                numberOfJumps++;
+            }
+        }
     }
 }
